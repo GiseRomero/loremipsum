938bde7a43810bcc14d45e0a131aaf90a45cbf69
GiseRomero <giseladelourdes.romero@gmail.com>
2023-04-30 14:23:36 -0300

    agrego contar.sh

--- contar.sh
+++ contar.sh
@@ -4,6 +4,6 @@ archivos_txt=$(ls *.txt)
 
 for archivo in $archivos_txt
 do
-    lineas=$(wc -l $archivo | cut -d' ' .f1)
+    lineas=$(wc -l $archivo | cut -d' ' -f1)
     echo "El archivo $archivo tiene $lineas líneas."
 done
